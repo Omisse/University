309a3adc98c7f1afc61e35f0f87af98d75119b0a
esgremar|Arch <kuuk@ALuminium.nomail>
2025-06-23 14:08:01 +0700

    all done

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
 
 //вспомогательная функция смены значений местами.
@@ -12,15 +11,17 @@ void swap_values(char* ptr1, char* ptr2)
 }
 
 //вывод перестановок
+//source - указатель на начало строки, всегда
+//locked_ptr - указатель на "зафиксированный" элемент
 int print_permutations(char* const source, char* const locked_ptr) 
 {
+    char* move_ptr = locked_ptr; //указатель на адрес, начиная от текущего зафиксированного
     //если на входе что-то не так - вернём ошибку
     if (!source || !locked_ptr) return 0;
     
     //если служебный указатель дошёл до конца строки - выведем итоговую строку
     if (!*locked_ptr) printf("%s\n",source);
     //начиная с позиции служебного указателя(при первом вызове - 0, далее - по элементам строки)
-    char* move_ptr = locked_ptr;
     //пока строка не кончилась
     while (*move_ptr)
     {   
@@ -38,9 +39,11 @@ int print_permutations(char* const source, char* const locked_ptr)
 
 int main() {
     setlocale(LC_ALL, "Russian");
+    // BUFSIZ - директива препроцессора из библиотеки stdio, определяющая размер буфера потока.
     char user_input[BUFSIZ] = {0};
     printf("Введите символы, не более %d:\n", BUFSIZ);
     scanf("%s", user_input);
+    //или fgets(user_input, BUFSIZ-1, stdin);
     print_permutations(user_input, user_input);
     return 0;
 }
