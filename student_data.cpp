d93a4016d16b9d278dbcfc67c0ee3899945b9f69
kuuk|Arch <kuuk@ALuminium.nomail>
2026-01-21 16:57:00 +0700

    no need for _memcpy since like lab1

--- student_data.cpp
+++ student_data.cpp
@@ -192,17 +192,10 @@ namespace s3l1 {
         if (src) {
             std::size_t size = _get_true_size(src);
             *dest = new char[size];
-            _memcpy(*dest, src, size);
+            memcpy(*dest, src, size);
             (*dest)[size-1] = 0;
         }
     }
-
-    void StudentData::_memcpy(char* dest, const char* src, std::size_t size) {
-        for (std::size_t i{0}; i < size; i++) {
-            dest[i] = src[i];
-        }
-    }
-
 
     StudentData& StudentData::operator++() {
         _age++;
@@ -264,7 +257,7 @@ namespace s3l1 {
         );
         
         _as_json = new char[new_size+1];
-        _memcpy(_as_json, new_json, new_size+1);
+        memcpy(_as_json, new_json, new_size+1);
     }
 
     std::size_t StudentData::_get_true_size(const char* str) {
--- student_data.h
+++ student_data.h
@@ -79,7 +79,6 @@ namespace s3l1 {
             bool _is_valid_age(const int age) const;
             bool _is_valid_average(const float average) const;
 
-            void _memcpy(char* dest, const char* src, std::size_t size);
             void _copy_string(char** dest,const char* src);
             void _update_json();
             static std::size_t _get_true_size(const char* str);
